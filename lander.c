6e8eec6daf78db707b8803480b2df761a666ed5a
nallen77 <nathan.allen8715@gmail.com>
2022-11-19 13:23:04 -0700

    copying .h functions into .c

--- lander.c
+++ lander.c
@@ -3,3 +3,22 @@
 lander_t the_lander;
 
 
+void lander_init();
+
+//calculates the x and y thrust values given the current angle
+int32_t get_thrust_y();
+int32_t get_thrust_x();
+
+//draw the lander at the current angle
+void draw_lander(display_point_t corner);
+
+//incrimient the angle 
+void lean_left();
+
+//decrement the angle
+void lean_right();
+
+void thrust();
+
+
+display_point_t get_location();
